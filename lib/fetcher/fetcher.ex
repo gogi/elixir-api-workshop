1ad45d8f0585ff5cd7a82c3e294c3b03ced25bd9
gogi <grobelska.m@gmail.com>
2016-12-04 15:46:24 +0100

    Add #fetcher to Fetcher

--- lib/fetcher/fetcher.ex
+++ lib/fetcher/fetcher.ex
@@ -1,3 +1,10 @@
 defmodule Apifrenzy.Fetcher do
 
+  def fetcher do
+    data_set = HTTPotion.get(source).body
+  end
+
+  def source do
+    "https://api-v3.mojepanstwo.pl/dane/kody_pocztowe"
+  end
 end
